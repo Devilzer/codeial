1c9243c5cdfe193c5ba87e1b8afaeef978708a20
Deepak Jena <jenadeepak724@gmail.com>
2020-07-22 18:23:05 +0530

    post controller

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user_controller');
+const postsController = require('../controllers/post_controller');
 
 module.exports = router;
 
@@ -9,3 +10,4 @@ router.get('/',(req,res) => {
     res.end('<h1>This is user controller</h1>');
 });
 router.get('/profile',userController.profile);
+router.get('/post',postsController.post);
