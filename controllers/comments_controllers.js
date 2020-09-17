55431626f9fd8b7a99d57cab98ea0ecb354b953a
Deepak Jena <jenadeepak724@gmail.com>
2020-09-17 19:36:43 +0530

    add async/await

--- controllers/comments_controllers.js
+++ controllers/comments_controllers.js
@@ -1,40 +1,39 @@
-const Comment = require('../models/comment');
-const Post = require('../models/post');
+const Comment = require("../models/comment");
+const Post = require("../models/post");
 
-module.exports.create = (req,res)=>{
-    Post.findById(req.body.post,(err,post)=>{
-        if(post){
-            Comment.create({
-                content : req.body.content,
-                post:req.body.post,
-                user:req.user._id
-            },(err,comment)=>{
-                if(err){
-                    console.log("error in creating comment");
-                }
-                post.comments.push(comment);
-                post.save();
-                res.redirect('/');  
-            }
-            );
-        }
-        if(err){
-            console.log("error in creating comment");
-        }
-    });
+module.exports.create = async (req, res) => {
+  try {
+    let post = await Post.findById(req.body.post);
+    if (post) {
+      let comment = await Comment.create({
+        content: req.body.content,
+        post: req.body.post,
+        user: req.user._id,
+      });
+      post.comments.push(comment);
+      post.save();
+      return res.redirect("/");
+    }
+  } catch (error) {
+    console.log("error in comment control create", error);
+  }
 };
-module.exports.destroy = (req,res) =>{
-    Comment.findById(req.params.id,(err,comment)=>{
-        if(comment.user == req.user.id){
-            let postId = comment.post;
-            comment.remove();
 
-            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},(err,post)=>{
-                return res.redirect('back');
-            });
-        }
-        else{
-            return res.redirect('back');
-        }
-    });
+//comment delete function
+module.exports.destroy = async (req, res) => {
+  try {
+    let comment = await Comment.findById(req.params.id);
+    if (comment.user == req.user.id) {
+      let postId = comment.post;
+      comment.remove();
+      await Post.findByIdAndUpdate(postId, {
+        $pull: { comments: req.params.id },
+      });
+      return res.redirect("back");
+    } else {
+      return res.redirect("back");
+    }
+  } catch (error) {
+    console.log("error in comment control destroy", error);
+  }
 };
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,29 +1,30 @@
-const Post = require('../models/post');
-const Comment = require('../models/comment');
+const Post = require("../models/post");
+const Comment = require("../models/comment");
 
-module.exports.create = (req,res)=>{
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    },(err,post)=>{
-        if(err)
-        {console.log("error in creating a post");
-        return;}
-        return res.redirect('back');
+module.exports.create = async (req, res) => {
+  try {
+    await Post.create({
+      content: req.body.content,
+      user: req.user._id,
     });
+    return res.redirect("back");
+  } catch (error) {
+    console.log("error in post control create", error);
+  }
 };
 
-module.exports.destroy = (req,res)=>{
-    Post.findById(req.params.id,(err,post)=>{
-        //.id to convert _id to string easier to compare
-        if(post.user == req.user.id){
-            post.remove();
-            Comment.deleteMany({post:req.params.id},(err)=>{
-                return res.redirect('back');
-            });
-        }
-        else{
-            return res.redirect('back');
-        }
-    });
+module.exports.destroy = async (req, res) => {
+  try {
+    let post = await Post.findById(req.params.id);
+    //.id to convert _id to string easier to compare
+    if (post.user == req.user.id) {
+      post.remove();
+      await Comment.deleteMany({ post: req.params.id });
+      return res.redirect("back");
+    } else {
+      return res.redirect("back");
+    }
+  } catch (error) {
+    console.log("error in post control destroy", error);
+  }
 };
