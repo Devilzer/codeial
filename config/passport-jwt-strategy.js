26d550503abf02f3b74000d4a7337dfae130ceb7
Deepak Jena <jenadeepak724@gmail.com>
2020-09-23 23:45:27 +0530

    Added Jwt strategy

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,28 @@
+const passpost = require("passport");
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+
+const User = require("../models/user");
+
+let opts = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,
+  secretOrKey: "codeial",
+};
+
+passpost.use(
+  new JWTstrategy(opts, (jwtPayLoad, done) => {
+    User.findById(jwtPayLoad._id, (err, user) => {
+      if (err) {
+        console.log("error in finding user JWT");
+        return;
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  })
+);
+
+module.exports = passpost;
--- routes/api/v1/post.js
+++ routes/api/v1/post.js
@@ -5,5 +5,6 @@ const postApi = require("../../../controllers/api/v1/post_api");
 const { post } = require("../../user");
 
 router.get("/", postApi.index);
+router.delete("/:id", postApi.destroy);
 
 module.exports = router;
